847e09c2c7aa97a8a6a54ef57dbbe13e3ce577e0
João Vitor da Silva <jdalpicio@outlook.com>
2023-10-14 13:45:16 -0300

    Update main.c

--- main.c
+++ main.c
@@ -14,8 +14,10 @@ typedef struct mesa
 typedef struct grupo
 {
     int qtdPessoasGrupo;
-    int idGrupo
+    int idGrupo;
+    struct grupo *prox;
 } Grupo;
+
 
 //  é possível usar uma lista de grupos, ao inserir ordenar por idGrupo.
 //  ao chegar um grupo, se tiver mais que 4 pessoas, criar um segundo nó com as pessoas restantes.
@@ -42,18 +44,101 @@ void setaMesasIniciais (Mesa *vetorMesas, int qtdLinhas, int qtdColunas)
     }
 }
 
+int perguntaLinhasColunas(int *qtdLinhas, int *qtdColunas)
+{
+
+    printf ("Informe linhas: ");
+    scanf ("%d", qtdLinhas);
+    printf ("Informe colunas: ");
+    scanf ("%d", qtdColunas);
+
+    return *qtdLinhas * *qtdColunas;
+}   
+
+Grupo* criaGrupo(int qtdPessoas)
+{
+    Grupo* novo = (Grupo*)malloc(sizeof(Grupo));
+
+    novo->qtdPessoasGrupo = qtdPessoas;
+    novo->idGrupo = 0;
+    novo->prox = NULL;
+}
+
+Grupo *criaSubGrupo(Grupo *listaGrupos, int qtdPessoas)
+{
+    Grupo *novo = (Grupo*)malloc(sizeof(Grupo));
+
+    if(qtdPessoas <= 4)
+    {
+        novo->qtdPessoasGrupo = qtdPessoas;
+        novo->idGrupo = 0;
+        novo->prox = listaGrupos;
+    }else{
+        novo->qtdPessoasGrupo = 4;
+        novo->idGrupo = 0;
+        novo->prox = listaGrupos;
+
+        novo = criaSubGrupo(novo, qtdPessoas - 4);
+    }
+
+    return novo;
+}
+
+void imprimeListaSubgrupos(Grupo *listaGrupos)
+{
+    Grupo *p;
+    int i = 1;
+
+    for(p = listaGrupos; p != NULL; p = p->prox){
+        printf("Grupo: %d, qtdPessoas: %d \n", i, p->qtdPessoasGrupo);
+        i++;
+    }
+}
+
+int perguntaQtdPessoas()
+{   
+    int qtdPessoas;
+    printf ("Informe a quantidade de pessoas do grupo: ");
+    scanf ("%d", &qtdPessoas);
+
+    return qtdPessoas;
+}
+
+void imprimeMesasOcupadas(Mesa *vetorMesas, int qtdLinhas, int qtdColunas)
+{
+    for (int i = 0; i < qtdLinhas; i++)
+    {
+        {
+            for (int j = 0; j < qtdColunas; j++)
+            {   
+                if(vetorMesas[i*qtdColunas+j].livre = LIV){
+                    int numeroMesa = i*qtdColunas+j+1;
+                    printf("Mesa %d livre\n", numeroMesa);
+                }
+            }
+        }
+    }
+}
+
 int main ()
 {
-    int qtdLinhas, qtdColunas, qtdMesas;
+    int qtdMesas, qtdLinhas, qtdColunas;
     Mesa *vetorMesas;
     
-    printf ("Informe linhas: ");
-    scanf ("%d", &qtdLinhas);
-    printf ("Informe colunas: ");
-    scanf ("%d", &qtdColunas);
-    qtdMesas = qtdLinhas * qtdColunas;
+    qtdMesas = perguntaLinhasColunas(&qtdLinhas, &qtdColunas);
 
     vetorMesas = alocaVetor (qtdMesas);
 
     setaMesasIniciais (vetorMesas, qtdLinhas, qtdColunas);
+    imprimeMesasOcupadas(vetorMesas, qtdLinhas, qtdColunas);
+    
+    int qtdPessoas = perguntaQtdPessoas();
+    Grupo* grupo;   
+    grupo = criaGrupo(qtdPessoas);
+
+    Grupo *listaSubGrupos = NULL;
+    listaSubGrupos = criaSubGrupo(listaSubGrupos, grupo->qtdPessoasGrupo);
+
+    imprimeListaSubgrupos(listaSubGrupos);
+    
 }
